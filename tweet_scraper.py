8c0c1791bc9084a5c08f71c8ad5cceec023c9cb4
Vincent Maes <32563747+VincentMaes26@users.noreply.github.com>
2019-09-23 15:07:38 +0200

    put tweet scraping section in separate python script

---
+++ tweet_scraper.py
@@ -0,0 +1,99 @@
+import tweepy
+import json
+import re
+import pandas as pd
+import logging
+import datetime
+
+# Returns true if retweet or reply
+def isRetweet(tweet):
+    if (hasattr(tweet, 'retweeted_status')):
+        return True
+
+# Preprocessing of tweet texts
+def format_tweet(tweet):
+    processed_tweet=""
+    if not isRetweet(tweet):
+        for word in tweet.text.split():
+            
+            # Removing URL from tweet
+            processed_word = re.sub('([^0-9A-Za-z \t])|(\w+:\/\/\S+)', ' ', word)
+            
+            # Remove all the special characters
+            processed_word = re.sub(r'\W', '', processed_word)
+
+            # remove all single characters
+            processed_word = re.sub(r'\s+[a-zA-Z]\s+', ' ', processed_word)
+
+            # Remove single characters from the start
+            processed_word = re.sub(r'\^[a-zA-Z]\s+', ' ', processed_word) 
+
+            # Substituting multiple spaces with single space
+            processed_word = re.sub(r'\s+', '', processed_word, flags=re.I)
+
+            # Removing prefixed 'b'
+            processed_word = re.sub(r'^b\s+', ' ', processed_word)
+
+            # Converting to Lowercase
+            processed_word = processed_word.lower()
+            processed_tweet= processed_tweet+" "+processed_word
+
+        return processed_tweet
+            
+    else:
+        return
+
+# Gets tweets from api based on query word
+def get_tweets():
+    # Gets authentication details from json file
+    with open("twitter_credentials.json", "r") as file:
+        creds = json.load(file)
+    # Authenticates and connects to API
+    auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+    auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    return api
+    
+# Exports tweets to csv in datasets folder    
+def store_tweets_to_csv(start_date, end_date):
+    # Gets tweets based on query
+    api = get_tweets()
+    query = "climate change -filter:retweets"
+    cursor = tweepy.Cursor(api.search, q= query, lang="en", since=start_date,  until=end_date).items(50000)
+    
+    # Filters out None objects
+    cursor = list(filter(None,cursor))
+
+    # Filters out None objects
+    cursor = list(filter(None,cursor))
+    
+    # Preprocesses the tweet texts
+    tweets = [format_tweet(tweet) for tweet in cursor]
+    tweets = list(filter(None, tweets))
+
+    # Gets username and creation date of tweet
+    usernames = [tweet.user.name for tweet in cursor]
+    usernames = list(filter(None, usernames))
+    
+    creation_dates = [tweet.created_at for tweet in cursor]
+    creation_dates = list(filter(None, creation_dates))
+        
+    list_for_dataframe = list(zip(tweets,usernames, creation_dates))
+
+    df = pd.DataFrame(list_for_dataframe, columns=["tweet","username", "creation date"])
+    month = start_date.split('-')[1]
+    day = start_date.split('-')[2]
+    if df.shape[0] == 0:
+        print("There has been an error. Dataframe tweets_week{}-{} is empty".format(day, month))
+    else:
+        print("The dataframe tweets_week{}-{} has been stored in the datasets folder".format(day, month))
+        df.to_csv(r'C:\stage\project\datasets\tweets_week{}-{}.csv'.format(day, month), index=False)
+
+if __name__ == "__main__":
+    start_week = datetime.date.today() - datetime.timedelta(days=7)
+    end_week = datetime.date.today()
+    store_tweets_to_csv(start_week, end_week)
+
+
+
+
