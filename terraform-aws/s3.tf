4ba244b9e9a94ec7cb0c673a0bfc5b6a175c2184
VincentMaes26 <vm261996@gmail.com>
2019-10-29 11:35:38 +0100

    Added lambda, s3 and sagemaker configuration (terraform) 1.0

--- terraform-aws/s3.tf
+++ terraform-aws/s3.tf
@@ -1,5 +1,11 @@
 #Manages s3 bucket
 resource "aws_s3_bucket" "ops-vw-interns-climate-perception-tweets" {
-  #bucket = "${aws_s3_bucket}"
-  #acl = "private"
+  bucket = "ops-vw-interns-climate-perception-tweets"
+  acl    = "private"
+  cors_rule {
+    allowed_headers = ["Authorization"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
 }
--- terraform-aws/tweet-collector.tf
+++ terraform-aws/tweet-collector.tf
@@ -1,58 +1,115 @@
 # Lambda function 
 resource "aws_lambda_function" "tweet-collector" {
   function_name = "tweet-collector"
-  handler = "lambda_function.lambda_handler"
-  runtime = "Python 3.7"
-  role = "${aws_iam_role.tweet-collector-role}"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "Python 3.7"
+  role          = "${aws_iam_role.tweet-collector-role}"
 
   layers = [
     "${aws_lambda_layer_version.tweepy_layer}",
+    "${aws_lambda_layer_version.AWSLambda-Python37-SciPy1x}",
     "${aws_lambda_layer_version.Klayers-python37-pandas}"
   ]
 
   environment {
     variables = {
-      tweepy_ACCESS_SECRET = "${var.tweepy_ACCESS_SECRET}"
-      tweepy_ACCESS_TOKEN = "${var.tweepy_ACCESS_TOKEN}"
-      tweepy_CONSUMER_KEY = "${var.tweepy_CONSUMER_KEY}"
+      tweepy_ACCESS_SECRET   = "${var.tweepy_ACCESS_SECRET}"
+      tweepy_ACCESS_TOKEN    = "${var.tweepy_ACCESS_TOKEN}"
+      tweepy_CONSUMER_KEY    = "${var.tweepy_CONSUMER_KEY}"
       tweepy_CONSUMER_SECRET = "${var.tweepy_CONSUMER_SECRET}"
     }
   }
 }
 
 
-# Role
+# IAM
 resource "aws_iam_role" "tweet-collector-role" {
-  
+  name = "tweet-collector-role-ucv89lpt"
+
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "logs:CreateLogGroup",
+            "Resource": "arn:aws:logs:eu-west-1:916245739953:*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:logs:eu-west-1:916245739953:log-group:/aws/lambda/tweet-collector:*"
+            ]
+        }
+    ]
+  }
+  EOF
+
+  tags = {
+    tag-key = ""
+  }
 }
 
-# Lambda layers
+
+resource "aws_iam_role_policy_attachment" "lambda-s3-full-access-attach" {
+  role       = "${aws_iam_role.tweet-collector-role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_policy" "lambda-s3-full-access" {
+  name = "AmazonS3FullAccess"
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*"
+        }
+    ]
+  }
+  EOF
+
+}
+
+# Layers
 resource "aws_lambda_layer_version" "tweepy_layer" {
-  
+  layer_name = "tweepy_layer"
 }
 
 resource "aws_lambda_layer_version" "pandas25-layer" {
-  
+  layer_name = "pandas25-layer"
 }
+
+resource "aws_lambda_layer_version" "AWSLambda-Python37-SciPy1x" {
+  layer_name = "AWSLambda-Python37-SciPy1x"
+}
+
 
 # Cloudwatch 
 resource "aws_cloudwatch_event_rule" "tweet-collector-trigger" {
-  name = "tweet-collector-trigger"
-  description = "Runs tweet-collector every hour"
+  name                = "tweet-collector-trigger"
+  description         = "Runs tweet-collector every hour"
   schedule_expression = "cron(0 0/1 ? * * *)"
 }
 
 resource "aws_cloudwatch_event_target" "target-tweet-collector-trigger" {
-  rule = "${aws_cloudwatch_event_rule.tweet-collector-trigger}"
+  rule      = "${aws_cloudwatch_event_rule.tweet-collector-trigger}"
   target_id = "tweet-collector"
-  arn = "${aws_lambda_function.tweet-collector.arn}"
+  arn       = "${aws_lambda_function.tweet-collector.arn}"
 }
 
 resource "aws_lambda_permission" "allow-cloudwatch-trigger" {
-  statement_id = "AllowExecutionFromCloudWatch"
-  action = "lambda:InvokeMethod"
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeMethod"
   function_name = "${aws_lambda_function.tweet-collector.function_name}"
-  principal = "events.amazonaws.com"
-  source_arn = "${aws_cloudwatch_event_rule.tweet-collector-trigger.arn}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.tweet-collector-trigger.arn}"
 }
 
