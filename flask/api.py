32f6d4a0e679b58e0fd2bd1e59799f1c01325064
VincentMaes26 <vm261996@gmail.com>
2019-11-26 12:12:45 +0100

    Added MNB model + commented code for future models

--- flask/api.py
+++ flask/api.py
@@ -17,15 +17,16 @@ def vectorizer_lemmatized(text):
 def vectorizer(text):
     return text.split()
 
-lr = joblib.load('lr.joblib')
-# mnb = joblib.load('mnb.joblib')
-# svc = joblib.load('svc.joblib')
+lr = joblib.load('models/lr.joblib')
+mnb = joblib.load('models/mnb.joblib')
+#svc = joblib.load('models/svc.joblib')
 # dt = joblib.load('dt.joblib')
 # rf = joblib.load('rf.joblib')
 # xgb = joblib.load('xgb.joblib')
 
 
-class MakePrediction(Resource):
+# Creating endpoint for each model
+class MakePredictionLR(Resource):
     @staticmethod
     def post():
         posted_data = request.get_json()
@@ -37,7 +38,68 @@ class MakePrediction(Resource):
         })
 
 
-api.add_resource(MakePrediction, '/predict')
+class MakePredictionMNB(Resource):
+    @staticmethod
+    def post():
+        posted_data = request.get_json()
+        text = posted_data['text']
+
+        prediction = mnb.predict([text])[0]
+        return jsonify({
+            'Prediction': prediction
+        })
+
+# class MakePredictionSVC(Resource):
+#     @staticmethod
+#     def post():
+#         posted_data = request.get_json()
+#         text = posted_data['text']
+#
+#         prediction = svc.predict([text])[0]
+#         return jsonify({
+#             'Prediction': prediction
+#         })
+
+# class MakePredictionDT(Resource):
+#     @staticmethod
+#     def post():
+#         posted_data = request.get_json()
+#         text = posted_data['text']
+#
+#         prediction = dt.predict([text])[0]
+#         return jsonify({
+#             'Prediction': prediction
+#         })
+
+# class MakePredictionRF(Resource):
+#     @staticmethod
+#     def post():
+#         posted_data = request.get_json()
+#         text = posted_data['text']
+#
+#         prediction = rf.predict([text])[0]
+#         return jsonify({
+#             'Prediction': prediction
+#         })
+
+# class MakePredictionXGB(Resource):
+#     @staticmethod
+#     def post():
+#         posted_data = request.get_json()
+#         text = posted_data['text']
+#
+#         prediction = xgb.predict([text])[0]
+#         return jsonify({
+#             'Prediction': prediction
+#         })
+
+# Adding endpoints to API
+api.add_resource(MakePredictionLR, '/logistic-regression-predict')
+api.add_resource(MakePredictionMNB, '/multinomial-naive-bayes-predict')
+# api.add_resource(MakePredictionSVC, '/multinomial-naive-bayes-predict')
+# api.add_resource(MakePredictionDT, '/decision-tree-predict')
+# api.add_resource(MakePredictionRF, '/random-forest-predict')
+# api.add_resource(MakePredictionXGB, '/xgb-predict')
 
 
 if __name__ == '__main__':
