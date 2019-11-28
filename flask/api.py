46705721e01734c1eddbe39764ec739a96e3da46
VincentMaes26 <vm261996@gmail.com>
2019-11-28 15:56:25 +0100

    Added decision tree model + swagger configuration

--- flask/api.py
+++ flask/api.py
@@ -1,14 +1,28 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Blueprint
 from flask_restful import Api, Resource
+from flask_swagger_ui import get_swaggerui_blueprint
 from sklearn.externals import joblib
 from nltk.stem import WordNetLemmatizer
 import nltk
-nltk.download('wordnet')
+#nltk.download('wordnet')
 
 app = Flask(__name__)
 api = Api(app)
 
+# Swagger configuration
+SWAGGER_URL = '/api-docs'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "classifier-model-api"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+# Helper functions & classifier models
 lemmatizer = WordNetLemmatizer()
 
 def vectorizer_lemmatized(text):
@@ -20,12 +34,12 @@ def vectorizer(text):
 lr = joblib.load('models/lr.joblib')
 mnb = joblib.load('models/mnb.joblib')
 #svc = joblib.load('models/svc.joblib')
-# dt = joblib.load('dt.joblib')
+dt = joblib.load('models/dt.joblib')
 # rf = joblib.load('rf.joblib')
 # xgb = joblib.load('xgb.joblib')
 
 
-# Creating endpoint for each model
+# Endpoint for each model
 class MakePredictionLR(Resource):
     @staticmethod
     def post():
@@ -60,16 +74,16 @@ class MakePredictionMNB(Resource):
 #             'Prediction': prediction
 #         })
 
-# class MakePredictionDT(Resource):
-#     @staticmethod
-#     def post():
-#         posted_data = request.get_json()
-#         text = posted_data['text']
-#
-#         prediction = dt.predict([text])[0]
-#         return jsonify({
-#             'Prediction': prediction
-#         })
+class MakePredictionDT(Resource):
+    @staticmethod
+    def post():
+        posted_data = request.get_json()
+        text = posted_data['text']
+
+        prediction = dt.predict([text])[0]
+        return jsonify({
+            'Prediction': prediction
+        })
 
 # class MakePredictionRF(Resource):
 #     @staticmethod
@@ -97,7 +111,7 @@ class MakePredictionMNB(Resource):
 api.add_resource(MakePredictionLR, '/logistic-regression-predict')
 api.add_resource(MakePredictionMNB, '/multinomial-naive-bayes-predict')
 # api.add_resource(MakePredictionSVC, '/multinomial-naive-bayes-predict')
-# api.add_resource(MakePredictionDT, '/decision-tree-predict')
+api.add_resource(MakePredictionDT, '/decision-tree-predict')
 # api.add_resource(MakePredictionRF, '/random-forest-predict')
 # api.add_resource(MakePredictionXGB, '/xgb-predict')
 
