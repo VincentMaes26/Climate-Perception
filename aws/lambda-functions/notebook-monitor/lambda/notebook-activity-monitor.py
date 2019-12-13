5332d4f17058c78538a578185ff371022cd9231e
Vincent Maes <32563747+VincentMaes26@users.noreply.github.com>
2019-12-13 13:30:50 +0100

    Updated to get local time

--- aws/lambda-functions/notebook-monitor/lambda/notebook-activity-monitor.py
+++ aws/lambda-functions/notebook-monitor/lambda/notebook-activity-monitor.py
@@ -1,11 +1,24 @@
 import boto3
+import datetime
+import pytz
 
 CLIENT = boto3.client('sagemaker')
 
 # Main function
 def lambda_handler(event, context):
-    if len(CLIENT.list_notebook_instances(StatusEquals='InService')["NotebookInstances"]) != 0:
-        CLIENT.stop_notebook_instance(NotebookInstanceName='Twitter-sentiment-analysis')
-        print("Notebook instance has been stopped")
-    else:
-        print("Notebook is not in service")
+    timezone = pytz.timezone('Europe/Brussels')
+    local_time = timezone.localize(datetime.datetime.now())
+    local_time = local_time.astimezone(timezone).time()
+    print(local_time)
+
+    stop_time = datetime.time(hour=20)
+    print(stop_time)
+    if local_time >= stop_time:
+        instances = CLIENT.list_notebook_instances(StatusEquals='InService')["NotebookInstances"]
+        if len(instances) != 0:
+            for instance in instances:
+                CLIENT.stop_notebook_instance(NotebookInstanceName=instance["NotebookInstanceName"])
+            print("Notebook instances have been stopped")
+        else:
+            print("No notebooks in service")
+
